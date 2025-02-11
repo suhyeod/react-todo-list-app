5c788696ad7339ee0c9d537a30de91b62a83ddad
choisuhyeok <suhyeok00@naver.com>
2025-02-11 22:14:27 +0900

    HTML CSS 완, js파일 -> jsx로 변경  2025.02.11 ver2

---
+++ src/components/Controls.jsx
@@ -0,0 +1,21 @@
+import './Controls.css'
+
+
+function Controls(){
+    return(
+        <div className='controls'>
+            <input type="text" className='input' />
+            <button className='button'>
+                추가
+            </button>
+            <select name="" id="" className='select'>
+                <option value="">전체</option>
+                <option value="">할 일</option>
+                <option value="">완료</option>
+            </select>
+        </div>
+    )
+}
+
+
+export default Controls;
